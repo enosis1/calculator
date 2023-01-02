5e74eb25341d75bc454a52ee25cf0035b4ff8d13
Mike J <mtjohns98@gmail.com>
2023-01-02 12:44:14 -0500

    Add math functions to script.js

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
 function add(x, y) {
 	let sum = 0;
 	return sum = x + y;
-};
+}
 
-function subtract(x, y) {
+function subtract (x, y) {
 	let sum = 0;
 	return sum = x - y;
-};
+}
 
-function multiply(x, y) {
+function multiply (x, y) {
 	let sum = 1;
-	return sum = x * y;
-};
+	return sum = x * y; 
+}
 
-function divide(x, y) {
+function divide (x, y) {
 	let sum = 1;
 	return sum = x / y;
-};
-
-function operate(operator, x, y) {
-	if (operator === "+") {
-		return add(x, y);
-	} else if (operator === '-') {
-		return subtract(x, y);
-	} else if (operator === '*') {
-		return multiply(x, y);
-	} else {
-		return divide(x, y);
-	}
-};
-
-let currentNumber = "";
-const numberButtons = document.querySelectorAll('.number');
-const display = document.querySelector('.display');
-
-numberButtons.forEach((btn) => {
-	btn.addEventListener('click', (e) => {
-		handleNumber(e.target.textContent);
-	});
-});
-
-function handleNumber(number) {
-	currentNumber += number;
-	display.textContent = currentNumber;
 }
