89499a95b7e3e05de8be00ef8966369d524c2cae
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-09-20 16:20:20 -0400

    Update styles.css

--- js/script.js
+++ js/script.js
@@ -16,3 +16,19 @@ function multiply(...nums) {
 function divide(a, b) {
   return a / b;
 }
+
+let operand1;
+let operator;
+let operand2;
+
+function operate(operator, operand1, operand2) {
+  if (operator === "+") {
+    return add(operand1, operand2);
+  } else if (operator === "-") {
+    return subtract(operand1, operand2);
+  } else if (operator === "*") {
+    return multiply(operand1, operand2);
+  } else if (operator === "/") {
+    return divide(operand1, operand2);
+  }
+}
