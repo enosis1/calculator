d5585cc90e701241920640e85142984c6b5fed34
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-09-20 08:45:59 -0400

    Add calculator functions to script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Functions for a basic calculator
+// Add, subtract, multiply, divide
+
+function add(...nums) {
+  return nums.reduce((sum, num) => sum + num, 0);
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(...nums) {
+  return nums.reduce((sum, num) => sum * num);
+}
+
+function divide(a, b) {
+  return a / b;
+}
