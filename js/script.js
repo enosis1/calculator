2cfb863afe1e2330d12534b5670b341e18c9f363
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-09-21 09:13:46 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,51 @@
 // Functions for a basic calculator
 // Add, subtract, multiply, divide
+let text = document.querySelector(".calc-output");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+const operands = document.querySelectorAll(".operand");
+
+clear.addEventListener("click", clearNumber);
+
+operands.forEach((operand) => {
+  operand.addEventListener("click", displayNumber);
+});
+
+let operand1 = 0;
+let operand2 = 0;
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    let operatorSelected = operator.textContent;
+  });
+});
+
+
+function operate(operator, operand1, operand2) {
+  if (operator === "+") {
+    return add(operand1, operand2);
+  } else if (operator === "-") {
+    return subtract(operand1, operand2);
+  } else if (operator === "*") {
+    return multiply(operand1, operand2);
+  } else if (operator === "/") {
+    return divide(operand1, operand2);
+  }
+}
+
+function displayNumber(e) {
+  if (text.textContent.length >= 11) {
+    return;
+  } else if (e.target.textContent === ".") {
+    text.textContent += e.target.textContent;
+  } else {
+    text.textContent += +e.target.textContent;
+  }
+}
+
+function clearNumber() {
+  text.textContent = "";
+}
 
 function add(...nums) {
   return nums.reduce((sum, num) => sum + num, 0);
@@ -15,46 +61,4 @@ function multiply(...nums) {
 
 function divide(a, b) {
   return a / b;
-}
-
-let operator;
-let operand1;
-let operand2;
-
-let operators = document.querySelectorAll(".operator");
-
-function operate(operator, operand1, operand2) {
-  if (operator === "+") {
-    return add(operand1, operand2);
-  } else if (operator === "-") {
-    return subtract(operand1, operand2);
-  } else if (operator === "*") {
-    return multiply(operand1, operand2);
-  } else if (operator === "/") {
-    return divide(operand1, operand2);
-  }
-}
-
-let text = document.querySelector(".calc-output");
-const clear = document.querySelector(".clear");
-const btns = document.querySelectorAll(".operand");
-
-clear.addEventListener("click", clearNumber);
-
-btns.forEach((btn) => {
-  btn.addEventListener("click", displayNumber);
-});
-
-function displayNumber(e) {
-  if (text.textContent.length >= 11) {
-    return;
-  } else if (e.target.textContent === ".") {
-    text.textContent += e.target.textContent;
-  } else {
-    text.textContent += +e.target.textContent;
-  }
-}
-
-function clearNumber() {
-  text.textContent = "";
 }
