a008d6d4151a74a005d28c479a7d02a3c26f2d58
Mike J <72291166+enosis1@users.noreply.github.com>
2026-03-12 21:18:47 -0400

    Create the operate function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let number1;
+let number2;
+let operator;
+
 function add(number1, number2) {
   return number1 + number2;
 }
@@ -9,4 +13,16 @@ function multiply(number1, number2) {
 }
 function divide(number1, number2) {
   return number1 / number2;
+}
+
+function operate(number1, operator, number2) {
+  if (operator === "+") {
+    return add(number1, number2);
+  } else if (operator === "-") {
+    return subtract(number1, number2);
+  } else if (operator === "x") {
+    return multiply(number1, number2);
+  } else {
+    return divide(number1, number2);
+  }
 }
